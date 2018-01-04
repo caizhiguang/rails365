9a051044c3c8253a40fa19827d7d912c6eb1451b
hfpp2012 yinsigan <ganweiliang886@gmail.com>
2018-01-04 20:43:54 +0800

    可以暂停视频

---
+++ app/assets/javascripts/video.js
@@ -0,0 +1,10 @@
+$(window).keypress(function(e) {
+  var video = document.getElementById("vid");
+  if (e.which == 32) {
+    if (video.paused == true)
+      video.play();
+    else
+      video.pause();
+  }
+  e.preventDefault();
+});
